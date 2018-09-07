d27cce30663403c42119c4af4ce22a366e408061
Sripathm22 <mishra60@purdue.edu>
2018-09-07 15:55:10 -0400

    added docs

---
+++ app/routes/user-router.js
@@ -0,0 +1,50 @@
+let express = require('express');
+let bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'CarnetUser',
+    host: 'localhost',
+    database: 'Carnet',
+    password: 'CarnetPassword',
+    port: 5432,
+});
+
+
+// Instantiate router
+
+let userRoutes = express.Router();
+
+
+/**
+ *
+ */
+userRoutes.post('/register', (req, res) => {
+
+    // Generate new random clientID and secret
+
+    let user = {};
+    user.clientID = req.body.userName;
+    user.clientSecret = req.body.userPassword;
+
+    // Encrypt client secret with blowfish before saving to database
+
+    user.clientSecret = bcrypt.hashSync(user.clientSecret, 10);
+
+    pool.query('INSERT INTO Users (index, key, value) VALUES ($1, $2)',
+        [user.clientID, user.clientSecret], (err, res) => {
+
+        if(err){
+            return res.send({
+                errorType: 'InternalError',
+                message: err,
+            });
+        }
+
+        return res.send({
+            message: 'Success',
+        })
+    })
+});
+
+module.exports = userRoutes;
